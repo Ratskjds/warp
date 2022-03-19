2152a0e9e647ead4693b432812b2fd189b1f3b52
Adarsh Goel <88939380+adarsh-goel@users.noreply.github.com>
2022-03-19 13:46:59 +0530

    Update wp-plus.py

--- wp-plus.py
+++ wp-plus.py
@@ -63,10 +63,14 @@ while True:
 		g = g + 1
 		os.system('cls' if os.name == 'nt' else 'clear')
 		print("")
-		sys.stdout.flush()
+		animation = ["[■□□□□□□□□□] 10%","[■■□□□□□□□□] 20%", "[■■■□□□□□□□] 30%", "[■■■■□□□□□□] 40%", "[■■■■■□□□□□] 50%", "[■■■■■■□□□□] 60%", "[■■■■■■■□□□] 70%", "[■■■■■■■■□□] 80%", "[■■■■■■■■■□] 90%", "[■■■■■■■■■■] 100%"] 
+		for i in range(len(animation)):
+			time.sleep(0.5)
+			sys.stdout.write("\r[+] Preparing... " + animation[i % len(animation)])
+			sys.stdout.flush()
 		print(f"\n[-] WORK ON ID: {referrer}")    
 		print(f"[:)] {g} GB has been successfully added to your account.")
-		print(f"[] Total: {g} Good {b} Bad")
+		print(f"[#] Total: {g} Good {b} Bad")
 		print("[*] After 15 seconds, a new request will be sent.")
 		time.sleep(15)
 	else:
