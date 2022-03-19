5bb65199eb6d848a1661778f489519a750745179
Adarsh Goel <88939380+adarsh-goel@users.noreply.github.com>
2022-03-19 13:33:47 +0530

    Update wp-plus.py

--- wp-plus.py
+++ wp-plus.py
@@ -66,7 +66,7 @@ while True:
 		animation = ["[■□□□□□□□□□] 10%","[■■□□□□□□□□] 20%", "[■■■□□□□□□□] 30%", "[■■■■□□□□□□] 40%", "[■■■■■□□□□□] 50%", "[■■■■■■□□□□] 60%", "[■■■■■■■□□□] 70%", "[■■■■■■■■□□] 80%", "[■■■■■■■■■□] 90%", "[■■■■■■■■■■] 100%"] 
 		for i in range(len(animation)):
 			time.sleep(0.5)
-			sys.stdout.write("\r[+] Preparing... " + animation[i % len(animation)])
+			print("\r[+] Preparing... " + animation[i % len(animation)])
 			sys.stdout.flush()
 		print(f"\n[-] WORK ON ID: {referrer}")    
 		print(f"[:)] {g} GB has been successfully added to your account.")
