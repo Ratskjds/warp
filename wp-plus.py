a1b34b8823685f9467aa9ca260f74d481102d438
Adarsh Goel <88939380+adarsh-goel@users.noreply.github.com>
2022-03-21 13:29:26 +0530

    Update wp-plus.py

--- wp-plus.py
+++ wp-plus.py
@@ -63,7 +63,7 @@ while True:
 	if result == 200:
 		g = g + 1
 		os.system('cls' if os.name == 'nt' else 'clear')
-		print("")
+		print("                       ")
 		animation = ["[■□□□□□□□□□] 10%","[■■□□□□□□□□] 20%", "[■■■□□□□□□□] 30%", "[■■■■□□□□□□] 40%", "[■■■■■□□□□□] 50%", "[■■■■■■□□□□] 60%", "[■■■■■■■□□□] 70%", "[■■■■■■■■□□] 80%", "[■■■■■■■■■□] 90%", "[■■■■■■■■■■] 100%"] 
 		for i in range(len(animation)):
 			time.sleep(0.5)
@@ -73,7 +73,8 @@ while True:
 		print(f"[:)] {g} GB has been successfully added to your account.")
 		print(f"[#] Total: {g} Good {b} Bad")
 		print("[*] After 15 seconds, a new request will be sent.")
-		time.sleep(15)
+		print("                       ")
+		time.sleep(10)
 	else:
 		b = b + 1
 		os.system('cls' if os.name == 'nt' else 'clear')
